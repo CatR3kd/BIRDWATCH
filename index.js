ac90a77e9ffe67b0b7d4946218d4fce42484bd4b
Oliver <63563918+CatR3kd@users.noreply.github.com>
2022-12-12 15:51:26 +0000

    Patch

--- index.js
+++ index.js
@@ -687,11 +687,15 @@ async function updateTips(){
   await fetch('https://catr3kd-tip-api.catr3kd.repl.co/all?replId=64cefdf4-f0a7-4643-a002-1423cf53324a', {method: 'GET'})
   .then(r => r.json().then(res => {
   	tippers = (res.users || []);
-  }));
+  })).catch((error) => {
+    console.error(error);
+  });
   await fetch('https://catr3kd-tip-api.catr3kd.repl.co/top?replId=64cefdf4-f0a7-4643-a002-1423cf53324a', {method: 'GET'})
   .then(r => r.json().then(res => {
   	topTipper = (res.topTipper || '');
-  }));
+  })).catch((error) => {
+    console.error(error);
+  });
 }
 
 updateTips();
