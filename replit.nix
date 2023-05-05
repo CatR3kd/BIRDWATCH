f0829c8050126c3eb5245788142b21f7a10e27e4
CatR3kd <63563918+CatR3kd@users.noreply.github.com>
2023-05-05 18:27:04 +0000

    added gitignore

--- replit.nix
+++ replit.nix
@@ -1,6 +1,7 @@
 { pkgs }: {
 	deps = [
-		pkgs.nodejs-16_x
+		pkgs.git config --global core.excludesfile ~/.gitignore_global
+  pkgs.nodejs-16_x
         pkgs.nodePackages.typescript-language-server
         pkgs.yarn
         pkgs.replitPackages.jest
